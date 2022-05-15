427cee2947940f9db61ee3481650a557b352166e
netzego <github@netzego.de>
2022-05-15 18:00:48 +0200

    feat: Add `<leader>tg` for telescope `gitmoji`

--- nvim/.config/nvim/lua/netzego/keymaps.lua
+++ nvim/.config/nvim/lua/netzego/keymaps.lua
@@ -48,6 +48,7 @@ map("n", "<space>b", ":Telescope buffers<CR>", opts)
 map("n", "<space>r", ":Telescope registers<CR>", opts)
 map("n", "<space>d", ":Telescope diagnostics<CR>", opts)
 map("n", "<space>te", ":Telescope symbols<CR>", opts)
+map("n", "<space>tg", ":lua require'telescope.builtin'.symbols{ sources = {'gitmoji'} }", opts)
 
 -- Fugitive
 -- map("n", "<space>g", ":G<CR>", opts)
