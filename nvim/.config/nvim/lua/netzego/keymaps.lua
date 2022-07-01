2e5491898ac13de0679cdba78594a14f3c6820b2
netzego <github@netzego.de>
2022-07-01 22:53:48 +0200

    feat: Add keymaps for toggle virtual text

--- nvim/.config/nvim/lua/netzego/keymaps.lua
+++ nvim/.config/nvim/lua/netzego/keymaps.lua
@@ -69,3 +69,15 @@ end, {})
 
 -- Spell
 vim.keymap.set({ "n" }, "<f1>", ":ToggleSpell<CR>", o)
+
+-- Toggle virtual text
+vim.api.nvim_create_user_command("ToggleVirtualText", function()
+  if vim.diagnostic.config()["virtual_text"] then
+    vim.diagnostic.config({ virtual_text = false })
+  else
+    vim.diagnostic.config({ virtual_text = true })
+  end
+end, {})
+
+-- Virtual text
+vim.keymap.set({ "n" }, "<f7>", ":ToggleVirtualText<CR>", o)
