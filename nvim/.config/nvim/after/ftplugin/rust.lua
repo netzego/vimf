ba6ec52cda95d0edd17c4cd6fb39f2d0f7f6b2c9
netzego <github@netzego.de>
2022-07-05 06:01:00 +0200

    feat: Add keymaps for exec rust buffers

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -4,7 +4,12 @@ vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
 
-vim.keymap.set("n", "<space>m", "<cmd>!cargo run %<cr>", { silent = true, buffer = true })
--- vim.keymap.set("n", "<space>M", "<cmd>TERMINAL<cr>i", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>m", "<cmd>!cargo run<CR>", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>M", "<cmd>terminal cargo run<cr>i", { silent = true, buffer = true })
+-- cargo mappings
+vim.keymap.set("n", "<space>cr", "<cmd>terminal cargo --run<cr>i", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>ct", "<cmd>terminal cargo test<cr>i", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>cb", "<cmd>terminal cargo run --bin %:t:r<cr>i", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>ce", "<cmd>terminal cargo run --example %:t:r<cr>i", { silent = true, buffer = true })
 -- vim.keymap.set("n", "<space>t", "<cmd>TEST %<cr>", { silent = true, buffer = true })
 -- vim.keymap.set("n", "<space>T", "<cmd>TEST VERBOSE %<cr>", { silent = true, buffer = true })
