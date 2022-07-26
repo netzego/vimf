d23aa9b720d555a52b014c6304da8289f1b99c3a
netzego <github@netzego.de>
2022-07-26 16:03:40 +0200

    fix(nvim): Change rust test keymap `<space>ct`
    * Change to only test lib tests

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<space>m", "<cmd>!cargo run<CR>", { silent = true, buffer =
 vim.keymap.set("n", "<space>M", "<cmd>terminal cargo run<cr>i", { silent = true, buffer = true })
 -- cargo mappings
 vim.keymap.set("n", "<space>cr", "<cmd>terminal cargo --run<cr>i", { silent = true, buffer = true })
-vim.keymap.set("n", "<space>ct", "<cmd>terminal cargo test<cr>i", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>ct", "<cmd>terminal cargo test --lib<cr>i", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>cb", "<cmd>terminal cargo run --bin %:t:r<cr>i", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>ce", "<cmd>terminal cargo run --example %:t:r<cr>i", { silent = true, buffer = true })
 -- vim.keymap.set("n", "<space>t", "<cmd>TEST %<cr>", { silent = true, buffer = true })
