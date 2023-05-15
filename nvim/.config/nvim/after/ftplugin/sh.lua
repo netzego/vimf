9bfbfc5cdd3910e64ae2fafdb1f481c3e5eac373
netzego <github@netzego.de>
2023-05-15 15:50:04 +0200

    add cliargs -a and -x to shellcheck mapping

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -9,4 +9,4 @@ local bats_opts = "--verbose-run --show-output-of-passing-tests"
 vim.keymap.set("n", "<space>m", "<cmd>!bash %<cr>", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>M", "<cmd>terminal bash ./%<cr>i", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>t", "<cmd>!bats " .. bats_opts .. " %<cr>", { silent = true, buffer = true })
-vim.keymap.set("n", "<space>s", "<cmd>!shellcheck %<cr>", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>s", "<cmd>!shellcheck -a -x %<cr>", { silent = true, buffer = true })
