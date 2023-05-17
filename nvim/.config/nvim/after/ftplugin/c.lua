f697a99a0f02fd95798e843c691c23a22e15b753
netzego <github@netzego.de>
2023-05-17 19:27:17 +0200

    src: add `m` keybind for c files

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -3,3 +3,5 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
+
+vim.keymap.set("n", "<space>m", "<cmd>!make %< && ./%< <cr>", { silent = true, buffer = true })
