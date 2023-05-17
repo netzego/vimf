72b67b608802a8dcbd96ac8db89cf2a6354e5426
netzego <github@netzego.de>
2023-05-17 19:28:43 +0200

    src: add tw = 76 and formatexpr = ""

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -3,6 +3,8 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
+vim.bo.textwidth = 76
+vim.bo.formatexpr = ""
 
 local bats_opts = "--verbose-run --show-output-of-passing-tests"
 
