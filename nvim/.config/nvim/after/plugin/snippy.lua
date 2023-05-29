0d04ae02b3da859b5abb86fb75a0963271326f0d
netzego <github@netzego.de>
2023-05-30 01:31:18 +0200

    src: change hl group

--- nvim/.config/nvim/after/plugin/snippy.lua
+++ nvim/.config/nvim/after/plugin/snippy.lua
@@ -2,7 +2,7 @@
 local snippy = require("snippy")
 snippy.setup({
   snippet_dirs = "~/.local/share/nvim/site/pack/packer/start/vim-snippets/snippets,~/.config/nvim/data/snippets",
-  hl_group = "Search",
+  hl_group = "Comment",
   mappings = {
     is = {
       ["<c-l>"] = "expand_or_advance",
