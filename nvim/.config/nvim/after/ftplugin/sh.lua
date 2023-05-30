f406e9ea1772877f320493870650f9c82b5f2131
netzego <github@netzego.de>
2023-05-30 14:49:59 +0200

    src: add makeprg and efm to use make the vim way

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -1,14 +1,13 @@
 -- sh.lua
-vim.bo.expandtab = true
-vim.bo.shiftwidth = 4
-vim.bo.softtabstop = 4
-vim.bo.tabstop = 4
 vim.bo.textwidth = 76
 vim.bo.formatexpr = ""
 
+vim.opt_local.makeprg = "bash %"
+vim.opt_local.errorformat = "%f: line %l: %m"
+
 local bats_opts = "--verbose-run --show-output-of-passing-tests"
 
-vim.keymap.set("n", "<space>m", "<cmd>!bash %<cr>", { silent = true, buffer = true })
+vim.keymap.set("n", "<space>m", "<cmd>make<cr>", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>M", "<cmd>terminal bash ./%<cr>i", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>t", "<cmd>!bats " .. bats_opts .. " %<cr>", { silent = true, buffer = true })
 vim.keymap.set("n", "<space>s", "<cmd>!shellcheck -a -x %<cr>", { silent = true, buffer = true })
