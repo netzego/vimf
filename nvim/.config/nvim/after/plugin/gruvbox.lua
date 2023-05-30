d0cee3e6e441e107d0cdc086722483992343021e
netzego <github@netzego.de>
2023-05-30 17:23:34 +0200

    src: fix ugly QuickFixLine settings

--- nvim/.config/nvim/after/plugin/gruvbox.lua
+++ nvim/.config/nvim/after/plugin/gruvbox.lua
@@ -1,5 +1,24 @@
 -- lua/config/gruvbox.lua
 -- https://github.com/ellisonleao/gruvbox.nvim
+
+-- color tweaks
+local overrides = {
+  light0_hard = "#eaf4d7",
+  light0 = "#e2f1c7",
+  light0_soft = "#d1e5bc",
+  light1 = "#c9dbb2",
+  light2 = "#9bbea1",
+  light3 = "#8eae93",
+  light4 = "#789284",
+}
+
+-- merge tweaks with palette
+local cols = vim.tbl_extend(
+  "keep",
+  overrides,
+  require("gruvbox/palette").colors
+)
+
 require("gruvbox").setup({
   undercurl = true,
   underline = true,
@@ -10,24 +29,16 @@ require("gruvbox").setup({
     operators = false,
     folds = true,
   },
-  strikethrough = true,
+  strikethrough = false,
   invert_selection = false,
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
   inverse = false, -- invert background for search, diffs, statuslines and errors
   contrast = "",   -- can be "hard", "soft" or empty string
-  palette_overrides = {
-    light0_hard = "#eaf4d7",
-    light0 = "#e2f1c7",
-    light0_soft = "#d1e5bc",
-    light1 = "#c9dbb2",
-    light2 = "#9bbea1",
-    light3 = "#8eae93",
-    light4 = "#789284",
-  },
+  palette_overrides = overrides,
   overrides = {
-    -- CursorLine = { bg = "" }
+    QuickFixLine = { fg = cols.dark0, bg = cols.light1, bold = false },
   },
   dim_inactive = false,
   transparent_mode = false,
