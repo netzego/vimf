70e4526772dc433f5797a348225468f2d2656855
netzego <github@netzego.de>
2023-10-04 09:59:57 +0200

    src: fix black statusline when view `:messages`

--- nvim/.config/nvim/after/plugin/gruvbox.lua
+++ nvim/.config/nvim/after/plugin/gruvbox.lua
@@ -27,8 +27,8 @@ require("gruvbox").setup({
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
-  inverse = false, -- invert background for search, diffs, statuslines and errors
-  contrast = "",   -- can be "hard", "soft" or empty string
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "",  -- can be "hard", "soft" or empty string
   palette_overrides = overrides,
   overrides = {
     QuickFixLine = { fg = overrides.dark0, bg = overrides.light1, bold = false },
