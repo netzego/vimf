d375781c856a39b45fe8ab937b0cfd461689eeea
netzego <github@netzego.de>
2023-10-30 17:16:16 +0100

    bulk: wip -- lazy stupid loading

---
+++ nvim/.config/nvim/ftplugin/c.lua
@@ -0,0 +1,18 @@
+-- ftplugin/c.lua
+
+local tw = 4
+local keymap_opts = { silent = true, buffer = true }
+
+vim.bo.shiftwidth = tw
+vim.bo.softtabstop = tw
+vim.bo.tabstop = tw
+vim.bo.expandtab = true
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*.c" },
+    callback = function(_)
+        vim.lsp.buf.format({ async = false })
+    end
+})
+
+vim.keymap.set("n", "<space>m", "<cmd>!make %< && ./%< <cr>", keymap_opts)
--- nvim/.config/nvim/lua/netzego/plugins.lua
+++ nvim/.config/nvim/lua/netzego/plugins.lua
@@ -1,21 +1,49 @@
 require("lazy").setup(
-{
-  -- gruvbox
-  {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme gruvbox]])
-	end,
-  },
-},
--- lazy settings
-{
-	ui = {
-		border = "none",
-		pills = false,
-	},
-}
-)
+    {
+        -- gruvbox
+        {
+            "ellisonleao/gruvbox.nvim",
+            lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+            priority = 1000, -- make sure to load this before all the other start plugins
+            config = function()
+                -- load the colorscheme here
+                vim.cmd([[colorscheme gruvbox]])
+            end,
+        },
+        -- lsp
+        {
+            'neovim/nvim-lspconfig',
+            dependencies = {
+                -- full signature help, docs and completion for the nvim lua api
+                'folke/neodev.nvim',
+            },
+        },
+        -- cmp
+        {
+            "hrsh7th/nvim-cmp",
+            -- load cmp on InsertEnter
+            -- event = "InsertEnter",
+            -- these dependencies will only be loaded when cmp loads
+            -- dependencies are always lazy-loaded unless specified otherwise
+            dependencies = {
+                -- luasnip
+                'L3MON4D3/LuaSnip',
+                'saadparwaiz1/cmp_luasnip',
+
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-nvim-lua",
+                -- "hrsh7th/cmp-buffer",
+                -- "dcampos/cmp-snippy",
+
+                -- Adds a number of user-friendly snippets
+                -- 'rafamadriz/friendly-snippets',
+                "honza/vim-snippets"
+            },
+        },
+        -- telescope
+        {
+            'nvim-telescope/telescope.nvim',
+            -- tag = '0.1.4',
+            dependencies = { 'nvim-lua/plenary.nvim' }
+        }
+    }, {})
