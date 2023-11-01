713c21597d999843e6390dad6bacc4ffbf12410d
netzego <github@netzego.de>
2023-11-01 13:05:54 +0100

    bulk: add init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,18 +1,20 @@
 -- init.lua
 
 -- set leader before lazy plugin
--- this is must be done before loading lazy keymaps
+-- this is must/should be done before loading lazy keymaps
 vim.api.nvim_set_keymap("n", ";", "<nop>", {})
+vim.api.nvim_set_keymap("n", "\\", "<nop>", {})
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ";"
 
--- loads all the jazz
-require("netzego/lazy")
-require("netzego/plugins")
-require("netzego/plug/gruvbox")
-require("netzego/plug/neodev")  -- should be loaded before lsp stuff??
-require("netzego/plug/luasnip") -- should be loaded before cmp??
-require("netzego/plug/cmp")
-require("netzego/plug/lsp")
-require("netzego/options") -- last but not least
-require("netzego/keymaps") -- last but not least
+require("netzego/lazy")              -- loads all the jazz
+require("netzego/opts")              -- last but not least
+require("netzego/keys")              -- last but not least
+
+require("netzego/keymap/clipboard")  -- last but not least
+require("netzego/keymap/buf")        -- last but not least
+
+require("netzego/autocmd/curline")   -- autocmd
+require("netzego/usercmd/listchars") -- toggle
+require("netzego/usercmd/spell")     -- autocmd
+require("netzego/usercmd/virttext")  -- autocmd
