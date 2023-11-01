e2ee064e34861224e51e9c743388003fa6580c5c
netzego <github@netzego.de>
2023-11-01 22:30:33 +0100

    wip: src: autoformat

--- nvim/.config/nvim/ftplugin/lua.lua
+++ nvim/.config/nvim/ftplugin/lua.lua
@@ -1,18 +1,18 @@
 -- ftplugin/lua.lua
 
 local tw           = 2
-local keymap_opts  = { silent = true, buffer = true }
+local keymap_opts  = { silent = true, buffer = true, }
 
 vim.bo.shiftwidth  = tw
 vim.bo.softtabstop = tw
 vim.bo.tabstop     = tw
 vim.bo.expandtab   = true
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*.lua" },
-    callback = function(_)
-        vim.lsp.buf.format({ async = false })
-    end
+vim.api.nvim_create_autocmd({ "BufWritePre", }, {
+  pattern = { "*.lua", },
+  callback = function(_)
+    vim.lsp.buf.format({ async = false, })
+  end,
 })
 
 vim.keymap.set("n", "<space>m", "<cmd>!lua %<cr>", keymap_opts)
