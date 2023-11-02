d6f9f986c862fb3347529efbb7e6657a624cf065
netzego <github@netzego.de>
2023-11-02 11:12:15 +0100

    wip: src: cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,18 +3,16 @@
 -- set leader before lazy plugin
 -- this is must/should be done before loading lazy keymaps
 vim.api.nvim_set_keymap("n", ";", "<nop>", {})
-vim.api.nvim_set_keymap("n", "\\", "<nop>", {})
+-- vim.api.nvim_set_keymap("n", "\\", "<nop>", {})
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ";"
 
-require("netzego/lazy")              -- loads all the jazz
-require("netzego/opts")              -- last but not least
-require("netzego/keys")              -- last but not least
-
-require("netzego/keymap/clipboard")  -- last but not least
-require("netzego/keymap/buf")        -- last but not least
-
-require("netzego/autocmd/curline")   -- autocmd
-require("netzego/usercmd/listchars") -- toggle
-require("netzego/usercmd/spell")     -- autocmd
-require("netzego/usercmd/virttext")  -- autocmd
+require("netzego/lazy")
+require("netzego/opts")
+require("netzego/keys")
+require("netzego/keymap/clipboard")
+require("netzego/keymap/buf")
+require("netzego/autocmd/curline")
+require("netzego/usercmd/listchars")
+require("netzego/usercmd/spell")
+require("netzego/usercmd/virttext")
