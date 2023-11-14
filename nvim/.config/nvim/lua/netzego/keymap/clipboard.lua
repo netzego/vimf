de99b4d0df401441e851052fb589a96f1b918b4f
netzego <github@netzego.de>
2023-11-14 23:18:14 +0100

    src: autoformat

--- nvim/.config/nvim/lua/netzego/keymap/clipboard.lua
+++ nvim/.config/nvim/lua/netzego/keymap/clipboard.lua
@@ -3,9 +3,9 @@
 -- The `star` register is the `xclip -o -selection primary` (XA_PRIMARY)
 -- The `plus` register is the `xclip -o -selection clipboard` (XA_CLIPBOARD)
 
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true, }
 
-vim.keymap.set({ "n", "v" }, "<space>p", '"+p', opts)
-vim.keymap.set({ "n", "v" }, "<space>P", '"+P', opts)
-vim.keymap.set({ "n", "v" }, "<space>y", '"+y', opts)
-vim.keymap.set({ "n", "v" }, "<space>Y", '"+y$', opts)
+vim.keymap.set({ "n", "v", }, "<space>p", '"+p', opts)
+vim.keymap.set({ "n", "v", }, "<space>P", '"+P', opts)
+vim.keymap.set({ "n", "v", }, "<space>y", '"+y', opts)
+vim.keymap.set({ "n", "v", }, "<space>Y", '"+y$', opts)
