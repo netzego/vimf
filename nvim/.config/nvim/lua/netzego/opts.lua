e06c154863214511bac43ab3b353d0845d9b93e1
netzego <github@netzego.de>
2023-11-14 23:22:04 +0100

    src: change `vim.opt` to `vim.o` calls

--- nvim/.config/nvim/lua/netzego/opts.lua
+++ nvim/.config/nvim/lua/netzego/opts.lua
@@ -1,20 +1,20 @@
 -- lua/netzego/options.lua
 
-vim.g.netrw_liststyle = 6
+local opt = vim.opt -- vim.opt.x = { ... }
 
 -- To ALWAYS use the clipboard for ALL operations
 -- (instead of interacting with the '+' and/or '*' registers explicitly)
 -- vim.opt.clipboard = "unnamedplus"
 
 -- switching buffers without saving
-vim.g.hidden = true
+vim.o.hidden = true
 
 -- Set highlight on search
 vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
-vim.wo.numberwidth = 4
+vim.wo.numberwidth = 5
 vim.wo.relativenumber = true
 
 -- Tabulator defaults
@@ -30,7 +30,9 @@ vim.o.mouse = ""
 vim.o.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+vim.o.undofile = true
+vim.o.history = 1024
+vim.o.cmdwinheight = 16 -- default 7
 
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
@@ -40,27 +42,27 @@ vim.o.smartcase = true
 --  vim.o.updatetime = 250
 
 --  Foldmethod
-vim.opt.foldmethod = "indent"
+vim.o.foldmethod = "indent"
 
 --  Always show the signcolumn
-vim.wo.signcolumn = "yes:1"
+vim.wo.signcolumn = "yes:2"
 
 --  Don't show mode in cmdline
-vim.opt.showmode = false
+vim.o.showmode = false
 
 -- Number of line to keep above and below the cursor
-vim.g.scrolloff = 8
+vim.o.scrolloff = 2
 
 -- Number of lines for the command-line
-vim.opt.cmdheight = 2
+vim.o.cmdheight = 2
 
 -- Set colorscheme (order is important here)
 vim.o.termguicolors = true
 vim.o.background = "light"
 
 -- heighlights the cursorline
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "both"
+vim.o.cursorline = true
+vim.o.cursorlineopt = "both"
 
 -- Set completeopt to have a better completion experience
 vim.g.completeopt = "menuone,noselect,noinsert"
@@ -68,14 +70,14 @@ vim.g.pumheight = 16
 vim.g.pumwidth = 16
 
 --  Foldlevel
-vim.opt.foldlevel = 8
+vim.o.foldlevel = 8
 
 --  Modeline
-vim.opt.modeline = true
+vim.o.modeline = true
 
 -- listchars
-vim.opt.listchars:append({ eol = "↵" })
-vim.opt.listchars:append({ tab = " »" })
-vim.opt.listchars:append({ trail = "·" })
-vim.opt.listchars:append({ space = "·" })
-vim.opt.list = false
+vim.opt.listchars:append({ eol = "↵", })
+vim.opt.listchars:append({ tab = " »", })
+vim.opt.listchars:append({ trail = "·", })
+vim.opt.listchars:append({ space = "·", })
+vim.o.list = false
