1aa1d21c36376a5a90b280e86d45d5e9a5771d5e
netzego <git@netzego.de>
2024-08-21 14:36:53 +0200

    src: add okcolors colorscheme

---
+++ nvim/.config/nvim/lua/plugins/okcolors.lua
@@ -0,0 +1,9 @@
+-- https://github.com/e-q/okcolors.nvim
+
+return {
+  "e-q/okcolors.nvim",
+  config = function()
+    -- load the colorscheme here
+    vim.cmd([[colorscheme okcolors]])
+  end,
+}
