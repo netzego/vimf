09006aa13f28791ea7962f3308f0f99588ec2a7e
netzego <git@netzego.de>
2024-08-29 10:38:24 +0200

    src: nonels: shfmt: add extra args

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -16,6 +16,7 @@ return {
         require("none-ls-shellcheck.code_actions"),
         -- shfmt
         null_ls.builtins.formatting.shfmt.with({
+          extra_args = { "--keep-padding", "--func-next-line", },
           extra_filetypes = { "bats", },
         }),
         -- c99
