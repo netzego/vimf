f8c1e43d87cc5911f4303d00e92994c91ad946bc
netzego <git@netzego.de>
2024-09-27 11:55:06 +0200

    src!: use vanila colorscheme w/ minor tweaks

--- nvim/.config/nvim/lua/netzego/opts.lua
+++ nvim/.config/nvim/lua/netzego/opts.lua
@@ -60,6 +60,12 @@ vim.o.cmdheight = 2
 vim.o.termguicolors = true
 vim.o.background = "light"
 
+vim.cmd([[hi Normal guibg=None]])
+vim.cmd([[hi StatusLine guibg=NvimLightGrey1]])
+vim.cmd([[hi StatusLineNC guibg=NvimLightGrey1]])
+vim.cmd([[hi CursorLine guibg=NvimLightGrey1]])
+vim.cmd([[hi TelescopeSelection guibg=NvimLightGrey1]])
+
 -- heighlights the cursorline
 vim.o.cursorline = true
 vim.o.cursorlineopt = "both"
--- nvim/.config/nvim/lua/plugins/okcolors.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/e-q/okcolors.nvim
-
-return {
-  "e-q/okcolors.nvim",
-  config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme okcolors]])
-  end,
-}
