738274f73ea0b640cdfb1c0f597efca0d43fcd36
netzego <github@netzego.de>
2026-04-15 16:19:46 +0200

    src: after/ftplugin/sh.lua: add makeprg and `<leader>l` keymap

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -8,9 +8,11 @@ vim.bo.softtabstop = tw
 vim.bo.tabstop     = tw
 vim.bo.expandtab   = true
 vim.bo.keywordprg  = ":Man"
+vim.bo.makeprg     = "shellcheck -f gcc %"
 
 map({ "n", }, "<leader>c", "<cmd>!shellcheck -f gcc %<cr>", opts)
 map({ "n", }, "<leader>m", "<cmd>!bash %<cr>", opts)
+map({ "n", }, "<leader>l", "<cmd>!shellcheck -W10 % | grep https<cr>", opts)
 -- map("n", "<space>M", "<cmd>terminal lua -i %<cr>i", opts)
 -- map("n", "<space>j", "<cmd>!luajit %<cr>", opts)
 -- map("n", "<space>J", "<cmd>terminal luajit -i %<cr>i", opts)
